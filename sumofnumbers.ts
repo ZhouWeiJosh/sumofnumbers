47a8131580cb267426effc26f9203d7013934be3
Joshua Chow <98932409+ZhouWeiJosh@users.noreply.github.com>
2025-09-20 18:58:56 -1000

    Initial Commit

---
+++ sumofnumbers.ts
@@ -0,0 +1,39 @@
+function sumFor(numbers: number[]): number {
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+
+  return sum;
+}
+
+function sumWhile(numbers: number[]): number {
+  let sum = 0;
+
+  while (numbers.length > 0) {
+    sum += numbers.pop()!;
+  }
+
+  return sum;
+}
+
+function sumRecursion(numbers: number[]): number {
+  let sum = 0;
+
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  sum = numbers.pop()! + sumRecursion(numbers);
+  return sum;
+}
+
+function sumTheFunctionalWay(numbers: number[]): number {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(`For loop sum: ${sumFor([1, 2, 3, 4])}`);
+console.log(`While loop sum: ${sumWhile([1, 2, 3, 4])}`);
+console.log(`Recursion sum: ${sumRecursion([1, 2, 3, 4])}`);
+console.log(`Functional sum: ${sumTheFunctionalWay([1, 2, 3, 4])}`);
